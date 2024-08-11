b83fc24271e9eb7ca5089f47677c0ad5bd1e9c6a
Sithum <sithumsandaruwan200123@gmail.com>
2024-08-11 17:56:17 +0530

    database connected.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const db = require('./db');
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT,(error ) => {
-    if(!error)
+app.get('/', async (req, res) => {
+    try {
+        const result = await db.query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(PORT, (error) => {
+    if (!error)
         console.log("Server is successfully running")
     else
         console.log("error occured")
---
+++ db.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser');
+const { query, text } = require('express');
+const {Pool} = require ('pg');
+
+const pool = new Pool({
+    user : 'postgres',
+    password : 'Sikka@323',
+    host : 'localhost',
+    port : '5432',
+    database : 'test_code_project_plurative'
+});
+
+module.exports = {
+    query : (text,params) => pool.query(text,params)
+}
