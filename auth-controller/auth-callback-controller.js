5cb62caa4351c5f3a9af704ec7bb91fab3d177d3
Sithum <sithumsandaruwan200123@gmail.com>
2024-08-21 11:32:28 +0530

    auth controller  file completed

--- auth-controller/auth-callback-controller.js
+++ auth-controller/auth-callback-controller.js
@@ -1,3 +1,27 @@
+require('dotenv').config();
+const { Issuer } = require("openid-client");
+const pool = require("../model/users");
+const {codeVerifier} = require("./auth-controller");
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 
-app.get('/auth-callback')
+exports.authCallBack = async (req, res) => {
+
+    const code = req.query;
+
+    const googleIssure = await Issuer.discover("https://accounts.google.com");
+
+    const client = new googleIssure.Client({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        redirect_uris: REDIRECT_URI,
+        response_types: 'code'
+    })
+
+    const tokens = await client.callback(REDIRECT_URI,{code},{code_verifier:codeVerifier});
+
+
+}
--- auth-controller/auth-controller.js
+++ auth-controller/auth-controller.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { OAuth2Client } = require("google-auth-library");
 const { generators, Issuer } = require("openid-client");
 
@@ -5,11 +6,10 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
-const OAuth2Client = new OAuth2Client(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI);
 
 let codeVerifier = '';
 
-app.get('/auth',async(req,res)=>{
+exports.auth= async(req, res) => {
 
     codeVerifier = generators.codeVerifier();
 
@@ -18,9 +18,21 @@ app.get('/auth',async(req,res)=>{
     const googleIssure = await Issuer.discover('https://accounts.google.com');
 
     const client = new googleIssure.Client({
-        client_id:CLIENT_ID,
-        cliend_secret:CLIENT_SECRET,
-        redirect_uris:REDIRECT_URI,
-        response_types:['code']
+        client_id: CLIENT_ID,
+        cliend_secret: CLIENT_SECRET,
+        redirect_uris: REDIRECT_URI,
+        response_types: ['code']
     })
-})
+
+    const url = client.authorizationUrl(
+        {
+            scope: 'openid email profile',
+            code_challenge: codeChallenge,
+            code_challenge_method: S256
+        }
+    )
+
+    res.redirect(url);
+};
+
+module.exports = {auth,codeVerifier};
