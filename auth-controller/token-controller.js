9b015a7d5c492dc2e1c78cadc37009e5f0455b34
Sithum <sithumsandaruwan200123@gmail.com>
2024-08-21 14:48:08 +0530

    token controller file developed

---
+++ auth-controller/token-controller.js
@@ -0,0 +1,11 @@
+const JWT = require('jsonwebtoken');
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+exports.JWT = (req, res) => {
+    const { access_token } = req.query;
+    const payload = { access_token };
+    const token = JWT.sign(payload, SECRET_KEY, { expiresIn: '30m' });
+
+    res.json(JWT);
+}
