7013ada7795025ec77080525275d9c3cff5c0438
Sithum <sithumsandaruwan200123@gmail.com>
2024-08-23 09:51:22 +0530

    token-controller & app-routes editede.

--- controller/token-controller.js
+++ controller/token-controller.js
@@ -2,7 +2,7 @@ const JWT = require('jsonwebtoken');
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
-exports.JWT = (req, res) => {
+exports.getToken = (req, res) => {
     const { access_token } = req.query;
     const payload = { access_token };
     const token = JWT.sign(payload, SECRET_KEY, { expiresIn: '30m' });
--- routes/app-routes.js
+++ routes/app-routes.js
@@ -5,10 +5,10 @@ const userControl = require("../controller/user-controller");
 const tokenControl = require("../controller/token-controller");
 const authCallBackControl = require("../controller/auth-callback-controller");
 
-router.get('/api/users',userControl.getUsers);
-router.get('/auth',authControl.auth);
-router.get('/token',tokenControl.JWT);
-router.get('/auth-callback',authCallBackControl.authCallBack);
+router.get('/api/users', userControl.getUsers);
+router.get('/auth', authControl.auth);
+router.get('/token', tokenControl.getToken);
+router.get('/auth-callback', authCallBackControl.authCallBack);
 
 module.exports = router;
 
